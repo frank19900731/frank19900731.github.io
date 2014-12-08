b96b44604420ec950b27e34525c21982ec407a71
frank19900731 <scr_0731@126.com>
2014-12-09 02:33:01 +0800

    Site updated at 2014-12-08 18:33:00 UTC

--- javascripts/wbcard/weiboflow.js
+++ javascripts/wbcard/weiboflow.js
@@ -52,7 +52,7 @@ if( mobile )
     loadPerScroll = 8;
 }
 
-jsonLoad("json/2014.json");
+jsonLoad("/json/2014.json");
 
 $(window).on('load', function() {
     $('.img_up').fancybox({
@@ -102,7 +102,7 @@ function jumpTo(year) {
         $('<div>').attr('id', 'main').appendTo($('#row'));
         $("html, body").animate({ scrollTop: 0 }, 120);
         currentIndex = 0;
-        jsonLoad("json/" + year + ".json");
+        jsonLoad("/json/" + year + ".json");
         currentYear = year;
     }
 }
